227c7acc002351988c2db083b78e16bc7b757ff9
Giles Dring <giles@dringtech.com>
2023-10-19 23:01:34 +0100

    Add deploy to github pages

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,6 +1,8 @@
-import '$lib/style.css';
 export const trailingSlash = 'always';
+export const prerender = true;
+export const ssr = false;
 
+import '$lib/style.css';
 import framework from '$lib/framework.json';
 
 /** @type {import('./$types').PageLoad} */
--- src/routes/framework/[...path]/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-  export let data;
-</script>
-
-<h2>Missing page</h2>
-
-<p>Page for <code>{ data.path }</code> not yet defined</p>
