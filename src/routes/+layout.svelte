0ba994a3b0858eb999aa03e87efbb55ed7d1c14c
Giles Dring <giles@dringtech.com>
2023-10-23 13:46:48 +0100

    Add some page animation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,28 @@
 <script>
-  import { base } from '$app/paths';
-  import '$lib/style.css';
+	import { base } from '$app/paths';
+	import '$lib/style.css';
+
+	import { onNavigate } from '$app/navigation';
+
+	onNavigate((navigation) => {
+		if (!document.startViewTransition) return;
+
+		return new Promise((resolve) => {
+			document.startViewTransition(async () => {
+				resolve();
+				await navigation.complete;
+			});
+		});
+	});
 </script>
 
-<div class='page-header'><header>
-	<h1><a href="{base}/">Social Value Data Explorer</a></h1>
-</header>
-<nav>
-	<ul><li><a href="{base}/framework/">Framework</a></li></ul>
-</nav>
+<div class="page-header">
+	<header>
+		<h1><a href="{base}/">Social Value Data Explorer</a></h1>
+	</header>
+	<nav>
+		<ul><li><a href="{base}/framework/">Framework</a></li></ul>
+	</nav>
 </div>
 <main>
 	<slot />
@@ -27,15 +41,16 @@
 	nav > ul {
 		list-style: none;
 	}
-  main {
-    padding-block: 2ch;
-  }
-  .page-header {
-    background: black;
-    color: white;
-    padding-block: 2ch;
-  }
-  .page-header a {
-    color: inherit;
-  }
+	main {
+		padding-block: 2ch;
+	}
+	.page-header {
+		background: black;
+		color: white;
+		padding-block: 2ch;
+    view-transition-name: header;
+	}
+	.page-header a {
+		color: inherit;
+	}
 </style>
