39f309269e670e626e56917c9e2f9a266db2da6e
Giles Dring <giles@dringtech.com>
2023-10-24 12:21:24 +0100

    Clean up duck initialisation

--- src/lib/duckdb.ts
+++ src/lib/duckdb.ts
@@ -1,24 +1,41 @@
 import * as duckdb from '@duckdb/duckdb-wasm';
-import duckdbWASM from '/node_modules/@duckdb/duckdb-wasm/dist/duckdb-mvp.wasm?url';
-import duckdbWorker from '/node_modules/@duckdb/duckdb-wasm/dist/duckdb-browser-mvp.worker.js?worker';
+import duckdb_wasm from '@duckdb/duckdb-wasm/dist/duckdb-mvp.wasm?url';
+import mvp_worker from '@duckdb/duckdb-wasm/dist/duckdb-browser-mvp.worker.js?url';
+import duckdb_wasm_eh from '@duckdb/duckdb-wasm/dist/duckdb-eh.wasm?url';
+import eh_worker from '@duckdb/duckdb-wasm/dist/duckdb-browser-eh.worker.js?url';
 
 import type { AsyncDuckDB } from '@duckdb/duckdb-wasm';
+
+const MANUAL_BUNDLES: duckdb.DuckDBBundles = {
+  mvp: {
+    mainModule: duckdb_wasm,
+    mainWorker: mvp_worker,
+  },
+  eh: {
+    mainModule: duckdb_wasm_eh,
+    mainWorker: eh_worker,
+  },
+};
+
 
 let db: AsyncDuckDB | null = null;
 
 const initDB = async () => {
-	if (db) {
-		return db; // Return existing database, if any
-	}
+  if (db) {
+    return db; // Return existing database, if any
+  }
 
-	// Instantiate worker
-	const logger = new duckdb.ConsoleLogger(duckdb.LogLevel.DEBUG);
-	const worker = new duckdbWorker();
+  // Select a bundle based on browser checks
+  const bundle = await duckdb.selectBundle(MANUAL_BUNDLES);
 
-	// and asynchronous database
-	db = new duckdb.AsyncDuckDB(logger, worker);
-	await db.instantiate(duckdbWASM);
-	return db;
+  // Instantiate worker
+  const logger = new duckdb.ConsoleLogger(duckdb.LogLevel.DEBUG);
+  const worker = new Worker(bundle.mainWorker!);
+
+  // and asynchronous database
+  db = new duckdb.AsyncDuckDB(logger, worker);
+  await db.instantiate(bundle.mainModule, bundle.pthreadWorker);
+  return db;
 };
 
 export { initDB }; // so we can import this elsewhere
